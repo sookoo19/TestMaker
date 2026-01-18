e71d93bbe7813d704bc5e1475ad28f8743b70bc4
sookoo19 <kouhei20001011@gmail.com>
2026-01-18 20:22:32 +0100

    1/18

--- my-app/database/migrations/2026_01_13_143511_create_question_choices_table.php
+++ my-app/database/migrations/2026_01_13_143511_create_question_choices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Question;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,14 @@ return new class extends Migration
         Schema::create('question_choices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->foreignIdFor(Question::class)
+                // 外部キーには必ずインデックスを付与
+                ->index()
+                // 外部キー制約を定義
+                ->constrained()
+                // 親が（所属元User）が削除されたら子（所有するTodo）を自動的に削除
+                ->cascadeOnDelete();
         });
     }
 
